e4973008f43c0d14ef18c39d57e7250ccf8915d6
rogo! <r480github@gmail.com>
2025-11-21 23:36:27 -0500

    particles

---
+++ public/assets/js/search.js
@@ -0,0 +1,30 @@
+import { setTransport, setWisp, makeURL, proxyUV } from "../../lithium.mjs";
+import("../../uv/uv.config.js");
+
+let iframe;
+let protocol = location.protocol === "https:" ? "wss://" : "ws://";
+let host = location.host;
+let input = document.getElementById("rogoisdabest");
+setWisp(`${protocol}${host}/wisp/`);
+setTransport("epoxy");
+
+document.addEventListener("keyup", async (e) => {
+  if (e.key === "Enter" || e.keyCode === 13) {
+    iframe = document.getElementById("frame");
+
+    if (
+      input.value.trim().includes(".") &&
+      !input.value.trim().startsWith("http://") &&
+      !input.value.trim().startsWith("https://")
+    ) {
+      input.value = "https://" + input.value;
+    }
+
+    let url = await proxyUV(makeURL(input.value));
+    console.log("set to UV");
+    iframe.src = url;
+    iframe.style.zIndex = "100";
+
+    console.log("Loading URL in", iframe.id, ":", url);
+  }
+});
